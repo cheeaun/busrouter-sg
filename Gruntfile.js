6f23ea574666d02a5bd728e1d25db585a142e638
Lim Chee Aun <cheeaun@gmail.com>
2013-07-28 17:43:11 +0800

    Upgrade JS scripts, use Grunt uglify for concat+min

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,16 @@ module.exports = function(grunt) {
 
  	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			scripts: {
+				options: {
+					sourceMap: 'js/scripts.js.map'
+				},
+				files: {
+					'js/scripts.js': ['assets/js/*.js']
+				}
+			}
+		},
 		connect: {
 			server: {
 				options: {
@@ -12,6 +22,7 @@ module.exports = function(grunt) {
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadTasks('tasks');
 
