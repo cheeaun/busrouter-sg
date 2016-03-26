5a4667af4116bd8d3230b8aa9f091e785e95e3de
Lim Chee Aun <cheeaun@techinasia.com>
2016-03-27 01:07:43 +0800

    Adjustment for search and geolocation buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -120,12 +120,7 @@
 				};
 
 				if (navigator.geolocation){
-					var geolocationControl = document.createElement('a');
-					geolocationControl.id = 'geolocation-control';
-					geolocationControl.href = '#';
-					geolocationControl.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24">'
-					  + '<path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/>'
-					+ '</svg>';
+					var geolocationControl = document.getElementById('geolocation-control');
 					map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(geolocationControl);
 
 					var locationMarker = new google.maps.Marker({
@@ -212,6 +207,9 @@
 						map.fitBounds(bounds);
 					}, 300);
 				});
+
+				var $headerSearch = document.getElementById('header-search');
+				map.controls[google.maps.ControlPosition.RIGHT_TOP].push($headerSearch);
 
 				var $form = $('#places-search-form');
 				$form.on('submit', function(e){
