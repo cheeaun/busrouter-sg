cf40fc8cf0c5137991d81f16fb4675758fa25638
Lim Chee Aun <cheeaun@gmail.com>
2018-11-01 23:43:31 +0800

    Fix all the abbreviations!

--- utils/titleCase.js
+++ utils/titleCase.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+const ABBRS = JSON.parse(fs.readFileSync('data/3/abbrs.json'));
+// Special use-case to for abbreviations used in Singapore
+
 module.exports = function(str){
-  return str.replace(/\w\S*/g, (s) =>  s.charAt(0).toUpperCase() + s.substr(1).toLowerCase());
+  return str.replace(/\w\S*/g, (s) => {
+    if (ABBRS.includes(s)){
+      return s;
+    }
+    return s.charAt(0).toUpperCase() + s.substr(1).toLowerCase()
+  });
 }
