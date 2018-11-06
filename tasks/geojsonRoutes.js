5409c841792912e3876937079bc354c7a76ae400
Lim Chee Aun <cheeaun@gmail.com>
2018-11-06 16:59:19 +0800

    Fix geojsonRoutes task

--- tasks/geojsonRoutes.js
+++ tasks/geojsonRoutes.js
@@ -1,19 +1,29 @@
 const fs = require('fs');
-const { encode } = require('../utils/specialID');
-
 const services = JSON.parse(fs.readFileSync('data/3/services.json'));
+
+const readFile = (path) => {
+  try {
+    return JSON.parse(fs.readFileSync(path));
+  } catch (e) {
+    return false;
+  }
+};
 
 const features = [];
 
 services.forEach(service => {
   const { no } = service;
-  const routes = JSON.parse(fs.readFileSync(`data/3/route-lines/${no}.json`));
+  const routes =
+    readFile(`data/3/routes/mytransportsg/${no}.json`) ||
+    readFile(`data/3/routes/towertransitsg/${no}.json`) ||
+    readFile(`data/3/routes/onemapsg/${no}.json`) ||
+    readFile(`data/3/routes/mapbox/${no}.json`);
   routes.forEach((route, i) => {
     features.push({
       type: 'Feature',
-      id: encode(`${i+1}${no}`),
       properties: {
         number: no,
+        route: i,
       },
       geometry: {
         type: 'LineString',
