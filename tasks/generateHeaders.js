ff2b283ae886e988f3dd83f467e7cb9d8ccc1b72
Lim Chee Aun <cheeaun@gmail.com>
2018-11-08 07:23:10 +0800

    Test generate _headers for http push

---
+++ tasks/generateHeaders.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const type = (filename) => {
+  if (/\.css$/.test(filename)) return 'style';
+  if (/\.js$/.test(filename)) return 'script';
+  if (/\.(jpe?g|png|gif|svg)$/.test(filename)) return 'image';
+};
+
+const files = fs.readdirSync('dist');
+
+const homeFiles = [
+  /^app\..+js$/,
+  /^app\..+css$/,
+  /^stop\..+png$/,
+  /^stop\-small\..+png$/
+];
+
+let content = '/*';
+
+files.forEach(f => {
+  if (homeFiles.some(r => r.test(f))){
+    content += `\n  Link: </${f}>; rel=preload; as=${type(f)}`;
+  }
+});
+
+const arrivalFiles = [
+  /^arrival\..+js$/,
+  /^arrival\..+css$/,
+  /^stop\-active\..+svg$/,
+  /^bus\-bendy\..+svg$/,
+  /^bus\-double\..+svg$/,
+  /^bus\-single\..+svg$/,
+  /^wheelchair\..+svg$/,
+];
+
+content += '\n/bus-arrival/*';
+
+files.forEach(f => {
+  if (arrivalFiles.some(r => r.test(f))){
+    content += `\n  Link: </${f}>; rel=preload; as=${type(f)}`;
+  }
+});
+
+fs.writeFileSync('dist/_headers', content);
