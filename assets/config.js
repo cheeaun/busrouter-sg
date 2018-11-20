85e25a222921190174cae677b357550b75cac6fd
Lim Chee Aun <cheeaun@gmail.com>
2018-11-21 00:15:31 +0800

    Use a specific access token for this, for tracking

--- assets/config.js
+++ assets/config.js
@@ -1,1 +1,1 @@
-export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiY2hlZWF1biIsImEiOiIwMTkyNjRiOWUzOTMyZThkYTE3YjMyMWFiZGU2OTZlNiJ9.XsOEKtyctGiNGNsmVhetYg';
+export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiY2hlZWF1biIsImEiOiJjam9weHRuMW4xdXczM3FteTR1OGt3OWxhIn0.I9fgZVxvN_wSJb8soniwpQ';
