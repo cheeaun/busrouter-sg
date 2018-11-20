0fc4006fc08a69114ccf65e1e3f9bceec9feeb70
Lim Chee Aun <cheeaun@gmail.com>
2018-11-20 08:12:06 +0800

    Legit fix for unregistering SW and make the redirect work

--- assets/app.js
+++ assets/app.js
@@ -2060,8 +2060,21 @@ class App extends Component {
 render(<App />, document.getElementById('app'));
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('../service-worker.js');
+  // Remove service worker
+  // - https://blog.hackages.io/migrating-a-service-worker-from-an-old-domain-to-your-new-domain-69236418051c
+  // - https://love2dev.com/blog/how-to-uninstall-a-service-worker/
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+      registration.unregister().then(function() {
+        return self.clients.matchAll();
+      }).then(function(clients) {
+        clients.forEach(client => {
+          if (client.url && 'navigate' in client){
+            client.navigate(client.url);
+          }
+        });
+      })
+    }
   });
 }
 
