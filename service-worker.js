f01eb1bb6e92889860fb76ed04896c2e5984626f
Lim Chee Aun <cheeaun@gmail.com>
2019-05-11 20:22:33 +0800

    Upgrade workbox

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 const url = new URL(location.href);
 const debug = url.searchParams.has('debug');
