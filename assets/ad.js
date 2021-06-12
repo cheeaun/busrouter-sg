59d4597ae06f3f06304626be4917bc2567504597
Lim Chee Aun <cheeaun@gmail.com>
2021-06-12 09:38:16 +0800

    Try different method

--- assets/ad.js
+++ assets/ad.js
@@ -1,5 +1,4 @@
 import { h } from 'preact';
-import { useEffect } from 'preact/hooks';
 
 export default () => {
   try {
@@ -7,11 +6,6 @@ export default () => {
       return '';
     }
   } catch (e) {}
-
-  useEffect(() => {
-    window.optimize = window.optimize || { queue: [] };
-    window.optimize.pushAll();
-  }, []);
 
   return <div id="bsa-zone_1623358681242-2_123456" style={{ backgroundColor: 'rgba(0,0,0,.03)' }} />;
 };
