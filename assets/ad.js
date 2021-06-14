60d54f961c2dec1b53af8962e8d4c380a02d47a8
Lim Chee Aun <cheeaun@gmail.com>
2021-06-14 23:44:23 +0800

    Fix ads not appearing

--- assets/ad.js
+++ assets/ad.js
@@ -1,11 +1,25 @@
-import { h } from 'preact';
+import { h } from "preact";
+import { useEffect } from "preact/hooks";
 
 export default () => {
   try {
-    if (localStorage['busroutersg.noads'] === '1') {
-      return '';
+    if (localStorage["busroutersg.noads"] === "1") {
+      return "";
     }
   } catch (e) {}
 
-  return <div id="bsa-zone_1623358681242-2_123456" class="ads" style={{ backgroundColor: 'rgba(0,0,0,.03)' }} />;
+  useEffect(() => {
+    window.optimize = window.optimize || { queue: [] };
+    window.optimize.queue.push(() => {
+      window.optimize.pushAll();
+    });
+  }, []);
+
+  return (
+    <div
+      id="bsa-zone_1623358681242-2_123456"
+      class="ads"
+      style={{ backgroundColor: "rgba(0,0,0,.03)" }}
+    />
+  );
 };
